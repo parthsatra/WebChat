a6dbf162aa77e9156e692ff10ded8ae2a1a2b75b
Parth Satra <psatra@gmail.com>
2015-04-24 22:04:25 -0400

    Adds comments to the code for better understanding"

--- app.js
+++ app.js
@@ -10,40 +10,60 @@ var users = require('./routes/users');
 
 var app = express();
 
+// creating the server on port 3000.
 var server = require('http').createServer(app);
 var port = 3000;
 server.listen(port);
 console.log("Server Socket started and listening on port : " + port);
 
+// initializing sockets
 var sio = require('socket.io').listen(server);
 
+// Maintains the map of data.user nickname and the corresponding socket id.
 var clients = {};
+// Maintains the map of socket ids and the corresponding nicknames. This is the inverse of the previous map.
 var socketsOfClients = {};
 
+// callback on socket connection from the client.
 sio.sockets.on('connection', function(socket) {
     console.log('Web client connected');
+    // Callback for register call from the client.
     socket.on('register', function(data) {
         console.log("Request Received");
+        // Check if the client name does not exists.
         if(clients[data.user] === undefined) {
+            // Assign the new nickname and set the corresponding socket id.
             clients[data.user] = socket.id;
             socketsOfClients[socket.id] = data.user;
             console.log("Sending response");
+            // Send the response to the client for a successful registration
             socket.emit('response', {status:true, text: "Welcome, " + data.user});
         } else if(clients[data.user] === socket.id) {
-            socket.emit('response', {status:true});
+            // If the client nickname is the same as saved with the right socket id then its just a reconnection.
+            // Hence a success message is sent back.
+            socket.emit('response', {status:true, text: "Welcome, " + data.user});
         } else {
+            // If the nickname exists with different socket Id,
+            // then return failure as the status since 2 users should have different nicknames.
             socket.emit('response', {status:false});
         }
     });
 
+    // Callback function when receiving chat message from the client.
     socket.on('chat', function(data) {
+        // Retrieves the nick name of the client who sent the message.
         var client = socketsOfClients[socket.id];
+        // Appends the clients nickname to the message
         var message = "<b>"+ client + "</b>: " + data.message
+        // Broadcasts the chat message to all the other clients connected to the server.
         sio.sockets.emit('message', {message: message});
     });
 
+    // Callback function called when the client disconnects.
     socket.on('disconnect', function() {
+        // getting the client name from the socketid.
         var name = socketsOfClients[socket.id];
+        // Deleting the client from both the clients list as well removing the socket ids.
         delete clients[name];
         delete socketsOfClients[socket.id];
         console.log('Web Client Disconnected');
--- public/javascripts/client.socket.io.js
+++ public/javascripts/client.socket.io.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
     $("#chat_div").hide();
 });
 
+// Simple trim function that removes empty spaces from the string.
+// This is used mainly for error handling and only sending a proper chat message.
 function myTrim(x) {
     return x.replace(/^\s+|\s+$/gm,'');
 }
@@ -21,55 +23,70 @@ var server_name = 'http://127.0.0.1:3000/';
 var server = io.connect(server_name);
 console.log('Client: Connecting to server '+ server_name);
 
+// On click function on registering. This function is called when user clicks on the register button.
 $("#register").click( function() {
-    // Send nick name to server
+    // Extract the nickname from the text field.
     var nickname = $("#nickname").val();
     if(myTrim(nickname).length != 0) {
+        // If the nickname is valid then sends the nickname to the server.
         server.emit('register', {user: nickname});
     } else {
+        // If the nickname is incorrect then just displays the error message.
         $("#error").text("Please enter valid nickname")
         $("#error").show();
     }
     $("#nickname").val("");
 });
 
+// Call back function form server on registration.
 server.on('response', function(data) {
     if(data.status == true) {
-        // Registration successful
+        // Registration successful.
+        // Thus hiding the registration div and showing the chat div.
         $("#nickname_div").hide();
         $("#error").hide();
         $("#window").show();
         $("#chat_div").show();
         $("#headerid").text(data.text);
     } else {
+        // Displays the appropriate error message from the server in case of a failure.
         $("#error").text("Nickname already exists. Please try a new one")
         $("#error").show();
         $("#nickname").val("");
     }
 });
 
+// On click function, which gets called on clicking on the send button.
 $("#send").click( function() {
+    // Extracts the chat message
     var message = $("#chat").val();
     if(myTrim(message).length != 0)  {
+        // If the message is not empty then send the message to the server.
         server.emit('chat', {message: message})
     }
     $("#chat").val("");
 });
 
+// Call back function from server to receive chats from other clients.
 server.on('message', function(data) {
+    // Kepping the size of the chat displayed to be limited.
     if($("#chatbody div").size() > 10) {
         $("#chatbody div:first").remove();
     }
-
+    // Adding the new chat message.
     $("#chatbody").append("<div class='chat-message' style='max-width:100%;' class='col-xs-12'>" + data.message + "</div>")
 });
 
+// Functions called on pressing enter on the text fields.
+// This is mainly done to register enter key as a special event which is to register it same as the button click.
+// Thus user when presses enter after typing the message the send button will be clicked automatically.
 $("#chat").keypress(function(e) {
    if(e.which === 13) {
        $('#send').click();
    }
 });
 
+// Similar enter key event for the nickname field.
 $("#nickname").keypress(function(e) {
    if(e.which === 13) {
        $('#register').click();
